6e56e7af7358312891ead8f24b04c66135e2ebb2
Conan Yuan <conanyuan@gmail.com>
2013-10-25 02:34:53 -0400

    Better standalone scripts

--- python/Save_Pairing.py
+++ python/Save_Pairing.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 
-import pairing
-
 if __name__ == "__main__":
-    pairing.save_pairing()
+    try:
+        # http://stackoverflow.com/questions/714063/python-importing-modules-from-parent-folder
+        import inspect
+        import os
+        import sys
+        currentdir = os.path.dirname(
+            os.path.abspath(inspect.getfile(inspect.currentframe())))
+        parentdir = os.path.dirname(currentdir)
+        sys.path.insert(0, os.path.join(parentdir, 'src'))
+        import pairing
+
+        pairing.save_pairing()
+
+    except Exception as e:
+        print "Error:"
+        print
+        print e
+        print
+        print "Type Control-C to Exit"
+        while True:
+            pass
