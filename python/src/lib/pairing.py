cff977b16641e2cad95a06a4b1b06f71f970a511
Conan Yuan <conanyuan@gmail.com>
2013-11-06 03:16:13 -0500

    add spell checker

--- python/src/lib/pairing.py
+++ python/src/lib/pairing.py
@@ -402,6 +402,58 @@ def log_to_file(file=LOG_FILE):
     logger.addHandler(fh)
 
 # -------------------------------------------------------
+
+class SpellChecker(object):
+    """
+    Given an attribute name, look for names with edit distance two or
+    less.  If there are none of those, look for names that start with
+    that attribute name (strike -> strike_price).
+
+    Based on http://norvig.com/spell-correct.html
+    """
+
+    @classmethod
+    def train(cls, features):
+        model = collections.defaultdict(lambda: 1)
+        for f in features:
+            model[f] += 1
+        return model
+
+    def __init__(self, wordlist, alphabet=None):
+        self.nwords = self.train(wordlist)
+        if alphabet is None:
+            alphabet = 'abcdefghijklmnopqrstuvwxyz_'
+        self.alphabet = alphabet
+
+    def edits1(self, word):
+        splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes    = [a + b[1:] for a, b in splits if b]
+        transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+        replaces   = [a + c + b[1:] for a, b in splits for c in self.alphabet
+                      if b]
+        inserts    = [a + c + b     for a, b in splits for c in self.alphabet]
+        return set(deletes + transposes + replaces + inserts)
+
+    def known_edits2(self, word):
+        return set(e2 for e1 in self.edits1(word) for e2 in self.edits1(e1)
+                   if e2 in self.nwords)
+
+    def known(self, words):
+        return set(w for w in words if w in self.nwords)
+
+    def starts(self, words):
+        return set(w for given in words for w in self.nwords
+                   if w.startswith(given))
+
+    def correct(self, word):
+        candidates = (self.known([word]) or
+                      self.known(self.edits1(word)) or
+                      self.known_edits2(word) or
+                      self.starts([word]) or
+                      [word])
+        return max(candidates, key=self.nwords.get)
+
+# -------------------------------------------------------
 # Pair and HistoricalData
 #
 # These are the heart of the data representation
