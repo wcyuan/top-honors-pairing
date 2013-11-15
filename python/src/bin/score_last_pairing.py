4fb8a3b8c890398446f57a1ff7f3384e2cb5473f
Conan Yuan <conanyuan@gmail.com>
2013-11-15 03:12:42 -0500

    add score_last_pairing.py

---
+++ python/src/bin/score_last_pairing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import inspect
+import os
+import sys
+import traceback
+
+def main():
+    try:
+        # http://stackoverflow.com/questions/714063/python-importing-modules-from-parent-folder
+        currentdir = os.path.dirname(
+            os.path.abspath(inspect.getfile(inspect.currentframe())))
+        parentdir = os.path.dirname(currentdir)
+        sys.path.insert(0, os.path.join(parentdir, 'lib'))
+        import pairing
+
+        pairing.score_historical_pairing()
+
+    except Exception as e:
+        print "Error:"
+        print
+        traceback.print_exc()
+        print
+        print "Type Control-C to Exit"
+        while True:
+            pass
+
+if __name__ == "__main__":
+    main()
